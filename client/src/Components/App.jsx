e3e03c30d75aa3521cb75f52fa7d0cdf83270a3a
nelliott82 <nikkoelliott@gmail.com>
2022-10-21 17:01:42 -0300

    Added Navbar component

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import Navbar from './Navbar';
 
 export default function App () {
   return (
-    <div>Hello.</div>
+    <>
+      <Navbar/>
+      <div>Hello.</div>
+    </>
   )
 };
