f5b6e8ee3bc0a5b245a4b680fff41c705f840ceb
nelliott82 <nikkoelliott@gmail.com>
2022-10-21 17:02:00 -0300

    Create Navbar component

---
+++ client/src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Navbar () {
+  return (
+    <div id='navbar'>
+      <div id='links'>
+        <p>Enter Time</p>
+      </div>
+      <div id='logout'>
+        <p>Logout</p>
+      </div>
+    </div>
+  )
+};
