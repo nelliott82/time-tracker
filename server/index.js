3ba854f31e95ff617aa8d6a271286b3a88e5ea74
nelliott82 <nikkoelliott@gmail.com>
2022-10-21 18:11:40 -0300

    Added tailwinds script and updated config file as well as classes to navbar

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 const path = require('path');
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(express.urlencoded({extended:true}));
 
 app.get('/', (req, res) => {
