f6261e88d60d787248e67af50b63ecdfa1cac834
nelliott82 <nikkoelliott@gmail.com>
2022-10-21 11:47:20 -0300

    Moved client files to a new client folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.join(__dirname, '/src/index.jsx'),
+  entry: path.join(__dirname, 'client/src/index.jsx'),
   output: {
     filename: 'bundle.js',
-    path: path.join(__dirname, '/dist')
+    path: path.join(__dirname, 'client/dist')
   },
   resolve: {
     extensions: ['.js', '.jsx'],
